9575bc2a236834e9db4208de3e623d272d4733d7
IAmChioma <37625087+IAmChioma@users.noreply.github.com>
2022-11-02 08:31:57 -0500

    Update settings.py
    Removed email host

--- mailme/settings.py
+++ mailme/settings.py
@@ -90,12 +90,6 @@ DATABASES = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'digfeb_pchioma@20191502'
-EMAIL_HOST_USER ='siddigitalsoln@gmail.com'
-EMAIL_HOST ='smtp.gmail.com'
 
 
 # Password validation
